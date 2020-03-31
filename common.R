b22e1bbe607f6d95f6ee41e7af15f0d262ab00a5
Juan Mayorga <js.mayorga368@gmail.com>
2020-03-31 16:43:16 -0700

    refined ocean raster

--- common.R
+++ common.R
@@ -8,9 +8,22 @@ emLab_shrd_data_dir <- "~/Google Drive File Stream/Shared drives/emlab/data"
 
 ocean_low_res_moll <- raster::raster(file.path(emLab_project_dir, "data", "02_processed", "masks", "ocean_low_res_moll.tif"), overwrite = T)
 
+ocean_low_res_wgs84 <- raster::raster(file.path(emLab_project_dir, "data", "02_processed", "masks", "ocean_low_res_wgs84.tif"), overwrite = T)
+
 land_50_moll <- sf::st_read(file.path(emLab_project_dir, "data", "02_processed", "masks", "land_50_moll.shp"))
 
+land_50_moll_raster <- raster::raster(file.path(emLab_project_dir, "data", "02_processed", "masks", "land_low_res_moll.tif"))
+
 land_50 <- sf::st_read(file.path(emLab_project_dir, "data", "02_processed", "masks", "land_50.shp"))
+
+# Ocean matrix
+
+ocean_matrix <- ocean_low_res_moll %>% 
+  raster::as.data.frame(xy = T) %>% 
+  filter(ocean_low_res_moll == 1) %>% 
+  mutate(cell_id = as.character(group_indices(., x, y))) %>% 
+  select(-ocean_low_res_moll) %>% 
+  as_tibble()
 
 # Basic mapping theme
 
@@ -24,3 +37,9 @@ my_theme_map <- function (base_size = 9, base_family = "") {
           panel.grid = element_line(color = "transparent"),
           legend.position = 'right', legend.margin = margin(t = 0, r = 0, b = 0, l = -.8, unit='cm'))
 }
+
+# Priorities mapping palette
+
+priorities_map_pal <- list(breaks = c(0, 0.2, 0.5, 0.7, 0.8, 0.85, 0.9, 0.95, 1),
+                           labels = c("0-20", "20-50", "50-70", "70-80", "80-85", "85-90", "90-95", "95-100"),
+                           colors = c("#4575b4", "#74add1", "#abd9e9", "#e0f3f8" ,"#fee090" ,"#fdae61" ,"#f46d43", "#d73027"))
